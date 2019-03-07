820d709be72d8b062c082ab91abe1dfe467e93d0
Sean Stephenson <Sean.M.Stephenson@espn.com>
2019-03-07 14:40:16 -0800

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+import Football from './Football'
+import Cycling from './Cycling'
+
+class Home extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Home
+            Home&nbsp;
+            <span role="img" aria-label="Home">
+              🏡
+            </span>
           </p>
+          <ul>
+            <li><strong>Home</strong></li>
+            <li><Link to="/football">Football</Link></li>
+            <li><Link to="/cycling">Cycling</Link></li>
+          </ul>
         </header>
       </div>
     );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/football" component={Football} />
+          <Route exact path="/cycling" component={Cycling} />
+        </>
+      </Router>
+    )
   }
 }
 
--- src/Cycling.jsx
+++ src/Cycling.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class Cycling extends Component {
@@ -7,11 +8,16 @@ class Cycling extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Cycling
+            Cycling&nbsp;
             <span role="img" aria-label="Cycling">
               🚲
             </span>
           </p>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/football">Football</Link></li>
+            <li><strong>Cycling</strong></li>
+          </ul>
         </header>
       </div>
     );
