2e571cefc78a3fc9da31f46cc9a97080ad31a727
Sean Stephenson <Sean.M.Stephenson@espn.com>
2019-03-07 14:26:53 -0800

    add football / cycling components

---
+++ src/Cycling.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Cycling extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Cycling
+            <span role="img" aria-label="Cycling">
+              🚲
+            </span>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Cycling;
---
+++ src/Football.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Football extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Football 
+            <span role="img" aria-label="Footabll">
+              🏈
+            </span>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Football;
